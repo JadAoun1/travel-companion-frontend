a5f01ddcb47f8407d1c1f4c42fe366d956850740
amenalii <ummeamen@gmail.com>
2025-04-13 04:26:21 -0500

    Trip Service created for index, able to view trips on dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,39 @@
 // src/components/Dashboard/Dashboard.jsx
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import * as tripService from "../../services/tripService";
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      try {
+        const fetchedTrips = await tripService.index();
+        setTrips(fetchedTrips);
+      } catch (error) {
+        console.error("Error fetching trips:", error);
+      }
+    };
+    if (user) {
+      fetchTrips();
+    }
+  }, [user]);
 
   return (
     <main>
       <h1>Trip Dashboard</h1>
       <p>Welcome, {user.username}</p>
-      <ul></ul>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip._id}>
+            {/* Maybe just keep trip title as a card on dashboard page and move description and other relevant details to the show page. */}
+            <h2>{trip.title}</h2>
+            <p>{trip.description}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -9,15 +9,18 @@ const Landing = () => {
     <main>
       <h1>App Title</h1>
       {/* Title Suggestions: Altoura, Aurevia, Trevia? Open to more ideas! */}
+
       {/* <h2>Discover, Plan, and Share Your Adventures</h2> */}
       {/* <h2>Plan trips, Make memories, Stay connected! </h2> */}
       <h2>Seamless planning for unforgettable escapes. </h2>
+
       <p>
         Whether you're planning a solo trip or a family vacation, our platform
         is here to help you every step of the way. From finding the perfect
         destination to sharing your memories, we make travel planning easy and
         enjoyable.
       </p>
+
       <button onClick={() => navigate("/sign-up")}>Sign Up</button>
       <button onClick={() => navigate("/sign-in")}>Sign In</button>
     </main>
