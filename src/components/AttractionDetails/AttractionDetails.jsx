d47f2713c51f520482e196a3209f2f4a31688606
annams <annaextrasteckel@gmail.com>
2025-04-15 16:52:40 -0700

    file structure setup with some barebones pseudocode

---
+++ src/components/AttractionDetails/AttractionDetails.jsx
@@ -0,0 +1,17 @@
+// src/components/AttractionDetails/AttractionDetails.jsx
+
+const AttractionDetails = () => {
+    // const
+
+    // useEffect
+
+    // if (!attraction)
+
+    return (
+        <>
+            <h1>Title</h1>
+        </>
+    );
+};
+
+export default AttractionDetails;
