1ccd866ae00c3c0e2ba4d0b9eef446e3ecc68c3a
annams <annaextrasteckel@gmail.com>
2025-04-15 16:57:00 -0700

    attractionService foundation set up

---
+++ src/services/attractionService.js
@@ -0,0 +1,15 @@
+// src/services/attractoinService.js
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/trips`;
+
+// index
+
+// create
+
+// show
+
+// edit
+
+// delete
+
+export {};
