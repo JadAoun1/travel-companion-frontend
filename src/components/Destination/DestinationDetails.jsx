61625e0b8a8c46fc05a39964debd30750824e017
annams <annaextrasteckel@gmail.com>
2025-04-15 14:06:26 -0700

    route updated to fetch destinations correctly

--- src/components/Destination/DestinationDetails.jsx
+++ src/components/Destination/DestinationDetails.jsx
@@ -1,46 +1,36 @@
 // src/components/Destination/DestinationDetails.jsx
 
-import { useParams, Link } from 'react-router';
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from "../../contexts/UserContext.jsx";
+import { useParams, useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
 
-import * as destinationService from '../../services/destinationService.js';
+import * as destinationService from "../../services/destinationService.js";
 
 const DestinationDetails = () => {
-    const { user } = useContext(UserContext);
-    // this isn't doing anything (destinationId is returning undefined)
-    const { destinationId } = useParams();
-    // not used (yet?); not sure what should be in useState() yet
-    const [destination, setDestination] = useState();
-    const [attraction, setAttraction] = useState();
+    const { tripId, destinationId } = useParams();
+    const [destination, setDestination] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchDestination = async () => {
-            try {
-                const fetchedDestination = await destinationService.index();
-                setDestination(fetchedDestination);
-            } catch (error) {
-                console.log(error);
-            };
+        const fetchDestinationDetails = async () => {
+            const destinationData = await destinationService.show(tripId, destinationId);
+            setDestination(destinationData);
         };
+        console.log(destinationId);
+        fetchDestinationDetails();
+    }, [destinationId]);
 
-        if (user) {
-            fetchDestination();
-            console.log(destinationId)
-        };
-        // Every time destinationId changes, we want to rerun (changed to user because that's passed, and destinationId isn't currently working)
-    }, [user]);
-
-    // Even though there is a destination, this is always rendering on the page...
-    // if (!destination) return <main>No destinations added...</main>
+    if (!destination) {
+        return <div>Looks like you haven't added any destinations yet!</div>
+    };
 
     return (
         <>
-            <h1>{user.username}'s Destination</h1>
+            <h1>{destination.name}</h1>
             <p>List all attractions here...</p>
             <p>Testing dynamic info here: {}</p>
         </>
     )
+
 };
 
 export default DestinationDetails;
