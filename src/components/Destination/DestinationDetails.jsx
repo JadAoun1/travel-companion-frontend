8b56eec70603f5ba8f3f5933617c4515bbbc4c65
annams <annaextrasteckel@gmail.com>
2025-04-14 17:41:59 -0700

    destination detail component barebones

---
+++ src/components/Destination/DestinationDetails.jsx
@@ -0,0 +1,26 @@
+// src/components/Destination/DestinationDetails.jsx
+
+import { userParams } from 'react-router';
+import { useEffect, useState } from 'react';
+
+const DestinationDetails = () => {
+    // const
+
+    // useEffect
+
+    // console.log()
+
+    return (
+        <main>
+            <section>
+                <header>
+                    <h1>Destinations Dashboard</h1>
+                    <p>See all destinations here</p>
+                </header>
+{/* show all attractions on this page. each should be linked to a new page with attraction details */}
+            </section>
+        </main>
+    );
+};
+
+export default DestinationDetails;
