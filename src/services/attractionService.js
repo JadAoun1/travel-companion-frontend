0c685e2cb3e97a4bf82c9b66bbdfedcfa28ae154
annams <annaextrasteckel@gmail.com>
2025-04-15 17:02:12 -0700

    show function created

--- src/services/attractionService.js
+++ src/services/attractionService.js
@@ -7,9 +7,19 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/trips`;
 // create
 
 // show
+const show = async (tripId, destinationId, attractionId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${tripId}/destinations/${destinationId}/attractions/${attractionId}`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        });
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 // edit
 
 // delete
 
-export {};
+export { show };
