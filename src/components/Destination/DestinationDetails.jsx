96b397b8924c5b0c8127ae308e9f9d40386dec27
annams <annaextrasteckel@gmail.com>
2025-04-15 19:39:25 -0700

    working on handleDeleteDestination

--- src/components/Destination/DestinationDetails.jsx
+++ src/components/Destination/DestinationDetails.jsx
@@ -12,7 +12,7 @@ const DestinationDetails = () => {
 
     useEffect(() => {
         const fetchDestinationDetails = async () => {
-            const destinationData = await destinationService.show(tripId, destinationId);
+            const destinationData = await destinationService.showDestination(tripId, destinationId);
             setDestination(destinationData);
         };
         console.log(destinationId);
@@ -23,16 +23,29 @@ const DestinationDetails = () => {
         return <div>Looks like you haven't added any destinations yet!</div>
     };
 
+    const handleDeleteDestination = async () => {
+        try {
+
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
     return (
         <>
             <h1>{destination.name}</h1>
+            {/* delete button */}
+            <button>Delete Destination</button>
+            {/* edit button to edit start and end dates */}
+            <button>Edit Destination</button>
             <h2>Attractions</h2>
             <div>
                 <ul>
                     {destination.attractions.map((attraction, index) => (
                         <li key={index}>
                             <p>{attraction.name}</p>
-                            <button onClick={() => navigate(`/trips/${tripId}/destinations/${destinationId}/attractions/${attractionId}`)}>View</button>
+                            {/* need to figure out how to pass attractionId forward onto this page (through props?) */}
+                            <button onClick={() => navigate(`/trips/${tripId}/destinations/${destinationId}/attractions/${attractionId}`)}>View Attraction</button>
                         </li>
                     ))}
                 </ul>
