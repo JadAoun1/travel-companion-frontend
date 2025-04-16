2d443d0a4d5065de37e573ab838d7c1c376b92e8
annams <annaextrasteckel@gmail.com>
2025-04-15 17:11:05 -0700

    attraction rendering on page

--- src/components/AttractionDetails/AttractionDetails.jsx
+++ src/components/AttractionDetails/AttractionDetails.jsx
@@ -1,15 +1,31 @@
 // src/components/AttractionDetails/AttractionDetails.jsx
 
+import { useParams } from 'react-router';
+import { useState, useEffect } from 'react';
+
+import * as attractionService from '../../services/attractionService.js';
+
 const AttractionDetails = () => {
-    // const
+    const { tripId, destinationId, attractionId } = useParams();
+    const [attraction, setAtraction] = useState(null);
 
-    // useEffect
+    useEffect(() => {
+        const fetchAttractionDetails = async () => {
+            const attractionDetails = await attractionService.show(tripId, destinationId, attractionId);
+            setAtraction(attractionDetails);
+        };
+        fetchAttractionDetails();
+    }, [attractionId]);
 
-    // if (!attraction)
+    if (!attraction) {
+        return <div>Looks like you haven't added any attractions yet!</div>
+    };
 
     return (
         <>
-            <h1>Title</h1>
+            <h1>{attraction.name}</h1>
+            <p>Lat: {attraction.location.lat}</p>
+            <p>Lng: {attraction.location.lng}</p>
         </>
     );
 };
