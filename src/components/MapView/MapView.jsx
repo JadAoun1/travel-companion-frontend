bb4c49a62b9f9a1ea575d411338dd54203ef5fbe
annams <annaextrasteckel@gmail.com>
2025-04-16 19:54:27 -0700

    MapView component fleshed out per docs guide; translated from typescript to jsx

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -1,12 +1,32 @@
 // src/components/MapView/MapView.jsx
 
 // imports
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 const MapView = () => {
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    // Handle changes to the view of the map
+    // event.detail contains { center, zoom, tilt, heading }
+    const handleCameraChange = (event) => {
+        console.log(event.detail);
+    };
+
+    if (!apiKey) {
+        return <div>Error: API key is missing.</div>
+    }
 
     return (
         <>
-            <h1>Hello world</h1>
+            <APIProvider
+                apiKey={apiKey}
+                onLoad={() => console.log('Maps API has loaded.')}>
+                <Map
+                    defaultZoom={13}
+                    defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+                    // Change view of map changes. 
+                    onCameraChanged={handleCameraChange}
+                    style={{ width: '100%', height: '500px' }} />
+            </APIProvider>
         </>
     );
 };
