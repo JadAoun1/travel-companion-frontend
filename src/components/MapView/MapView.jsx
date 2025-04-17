3fc577464c8eb5fe97047ce37a019731ae68c8d1
annams <annaextrasteckel@gmail.com>
2025-04-16 19:32:02 -0700

    MapView component created to display google api map

---
+++ src/components/MapView/MapView.jsx
@@ -0,0 +1,14 @@
+// src/components/MapView/MapView.jsx
+
+// imports
+
+const MapView = () => {
+
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+};
+
+export default MapView;
